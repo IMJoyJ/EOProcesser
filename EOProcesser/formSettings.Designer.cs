5ea434c57654c6cde6c74004bf73bf09b0a6dfdc
JoyJ <316351048@qq.com>
2025-08-13 02:16:12 +0800

    refactor(UI): rename button1 to btnSelectCardFolder and add click handler

--- EOProcesser/formSettings.Designer.cs
+++ EOProcesser/formSettings.Designer.cs
@@ -33,36 +33,36 @@
             btnSelectRootFolder = new Button();
             btnConfirm = new Button();
             btnCancel = new Button();
-            button1 = new Button();
+            btnSelectCardFolder = new Button();
             label2 = new Label();
             txtCardFolder = new TextBox();
             SuspendLayout();
             // 
             // txtRootFolder
             // 
-            txtRootFolder.Location = new Point(94, 6);
-            txtRootFolder.Margin = new Padding(1, 2, 1, 2);
+            txtRootFolder.Location = new Point(212, 12);
+            txtRootFolder.Margin = new Padding(2, 4, 2, 4);
             txtRootFolder.Name = "txtRootFolder";
-            txtRootFolder.Size = new Size(352, 27);
+            txtRootFolder.Size = new Size(787, 46);
             txtRootFolder.TabIndex = 0;
             txtRootFolder.TextChanged += txtRootFolder_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 8);
-            label1.Margin = new Padding(1, 0, 1, 0);
+            label1.Location = new Point(27, 16);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(85, 20);
+            label1.Size = new Size(179, 39);
             label1.TabIndex = 1;
             label1.Text = "Root folder";
             // 
             // btnSelectRootFolder
             // 
-            btnSelectRootFolder.Location = new Point(447, 3);
-            btnSelectRootFolder.Margin = new Padding(1, 2, 1, 2);
+            btnSelectRootFolder.Location = new Point(1006, 6);
+            btnSelectRootFolder.Margin = new Padding(2, 4, 2, 4);
             btnSelectRootFolder.Name = "btnSelectRootFolder";
-            btnSelectRootFolder.Size = new Size(31, 30);
+            btnSelectRootFolder.Size = new Size(70, 58);
             btnSelectRootFolder.TabIndex = 2;
             btnSelectRootFolder.Text = "...";
             btnSelectRootFolder.UseVisualStyleBackColor = true;
@@ -71,10 +71,10 @@
             // btnConfirm
             // 
             btnConfirm.DialogResult = DialogResult.OK;
-            btnConfirm.Location = new Point(399, 75);
-            btnConfirm.Margin = new Padding(1, 2, 1, 2);
+            btnConfirm.Location = new Point(898, 146);
+            btnConfirm.Margin = new Padding(2, 4, 2, 4);
             btnConfirm.Name = "btnConfirm";
-            btnConfirm.Size = new Size(38, 30);
+            btnConfirm.Size = new Size(86, 58);
             btnConfirm.TabIndex = 3;
             btnConfirm.Text = "√";
             btnConfirm.UseVisualStyleBackColor = true;
@@ -82,51 +82,52 @@
             // btnCancel
             // 
             btnCancel.DialogResult = DialogResult.Cancel;
-            btnCancel.Location = new Point(440, 75);
-            btnCancel.Margin = new Padding(1, 2, 1, 2);
+            btnCancel.Location = new Point(990, 146);
+            btnCancel.Margin = new Padding(2, 4, 2, 4);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(38, 30);
+            btnCancel.Size = new Size(86, 58);
             btnCancel.TabIndex = 4;
             btnCancel.Text = "×";
             btnCancel.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnSelectCardFolder
             // 
-            button1.Location = new Point(447, 34);
-            button1.Margin = new Padding(1, 2, 1, 2);
-            button1.Name = "button1";
-            button1.Size = new Size(31, 30);
-            button1.TabIndex = 7;
-            button1.Text = "...";
-            button1.UseVisualStyleBackColor = true;
+            btnSelectCardFolder.Location = new Point(1006, 66);
+            btnSelectCardFolder.Margin = new Padding(2, 4, 2, 4);
+            btnSelectCardFolder.Name = "btnSelectCardFolder";
+            btnSelectCardFolder.Size = new Size(70, 58);
+            btnSelectCardFolder.TabIndex = 7;
+            btnSelectCardFolder.Text = "...";
+            btnSelectCardFolder.UseVisualStyleBackColor = true;
+            btnSelectCardFolder.Click += btnSelectCardFolder_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 39);
-            label2.Margin = new Padding(1, 0, 1, 0);
+            label2.Location = new Point(27, 76);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(84, 20);
+            label2.Size = new Size(177, 39);
             label2.TabIndex = 6;
             label2.Text = "Card folder";
             // 
             // txtCardFolder
             // 
-            txtCardFolder.Location = new Point(94, 37);
-            txtCardFolder.Margin = new Padding(1, 2, 1, 2);
+            txtCardFolder.Location = new Point(212, 72);
+            txtCardFolder.Margin = new Padding(2, 4, 2, 4);
             txtCardFolder.Name = "txtCardFolder";
-            txtCardFolder.Size = new Size(352, 27);
+            txtCardFolder.Size = new Size(787, 46);
             txtCardFolder.TabIndex = 5;
             txtCardFolder.TextChanged += txtCardFolder_TextChanged;
             // 
             // formSettings
             // 
             AcceptButton = btnConfirm;
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(18F, 39F);
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = btnCancel;
-            ClientSize = new Size(483, 116);
-            Controls.Add(button1);
+            ClientSize = new Size(1087, 226);
+            Controls.Add(btnSelectCardFolder);
             Controls.Add(label2);
             Controls.Add(txtCardFolder);
             Controls.Add(btnCancel);
@@ -135,6 +136,7 @@
             Controls.Add(label1);
             Controls.Add(txtRootFolder);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(7, 6, 7, 6);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "formSettings";
@@ -150,7 +152,7 @@
         private Button btnSelectRootFolder;
         private Button btnConfirm;
         private Button btnCancel;
-        private Button button1;
+        private Button btnSelectCardFolder;
         private Label label2;
         private TextBox txtCardFolder;
     }
