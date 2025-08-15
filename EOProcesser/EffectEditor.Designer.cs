77a53d992f40f8df28bf6cc4cb4112915dc67506
JoyJ <316351048@qq.com>
2025-08-15 12:18:19 +0900

    feat(EOProcesser): add EffectEditor component for code editing
    Add new EffectEditor user control with tree-based code editing functionality. The component includes:
    - Split panel interface with code tree view and text editor
    - Context menu for adding, editing, and deleting code nodes
    - Node manipulation buttons for adding, removing, and reordering
    - File saving capabilities
    - Code indentation handling

---
+++ EOProcesser/EffectEditor.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace EOProcesser
+{
+    partial class EffectEditor
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            scContainer = new SplitContainer();
+            btnSaveFile = new Button();
+            treeCodeTree = new TreeView();
+            btnSave = new Button();
+            txtCode = new TextBox();
+            menuCodeMenu = new ContextMenuStrip(components);
+            ParentAddCodeToolStripMenuItem = new ToolStripMenuItem();
+            ParentAddCodeBeforeToolStripMenuItem = new ToolStripMenuItem();
+            ParentAddCodeAfterToolStripMenuItem = new ToolStripMenuItem();
+            ChildAddCodeToolStripMenuItem = new ToolStripMenuItem();
+            EditCodeToolStripMenuItem = new ToolStripMenuItem();
+            CodeDeleteToolStripMenuItem = new ToolStripMenuItem();
+            btnAddNewNode = new Button();
+            btnRemoveNode = new Button();
+            btnNodeMoveUp = new Button();
+            btnNodeMoveDown = new Button();
+            ((System.ComponentModel.ISupportInitialize)scContainer).BeginInit();
+            scContainer.Panel1.SuspendLayout();
+            scContainer.Panel2.SuspendLayout();
+            scContainer.SuspendLayout();
+            menuCodeMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // scContainer
+            // 
+            scContainer.Dock = DockStyle.Fill;
+            scContainer.Location = new Point(0, 0);
+            scContainer.Name = "scContainer";
+            scContainer.Orientation = Orientation.Horizontal;
+            // 
+            // scContainer.Panel1
+            // 
+            scContainer.Panel1.Controls.Add(btnNodeMoveDown);
+            scContainer.Panel1.Controls.Add(btnNodeMoveUp);
+            scContainer.Panel1.Controls.Add(btnRemoveNode);
+            scContainer.Panel1.Controls.Add(btnAddNewNode);
+            scContainer.Panel1.Controls.Add(btnSaveFile);
+            scContainer.Panel1.Controls.Add(treeCodeTree);
+            // 
+            // scContainer.Panel2
+            // 
+            scContainer.Panel2.Controls.Add(btnSave);
+            scContainer.Panel2.Controls.Add(txtCode);
+            scContainer.Size = new Size(633, 514);
+            scContainer.SplitterDistance = 211;
+            scContainer.TabIndex = 0;
+            // 
+            // btnSaveFile
+            // 
+            btnSaveFile.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSaveFile.Location = new Point(502, 179);
+            btnSaveFile.Name = "btnSaveFile";
+            btnSaveFile.Size = new Size(128, 29);
+            btnSaveFile.TabIndex = 2;
+            btnSaveFile.Text = "ファイル上書き";
+            btnSaveFile.UseVisualStyleBackColor = true;
+            btnSaveFile.Click += btnSaveFile_Click;
+            // 
+            // treeCodeTree
+            // 
+            treeCodeTree.Dock = DockStyle.Fill;
+            treeCodeTree.Location = new Point(0, 0);
+            treeCodeTree.Name = "treeCodeTree";
+            treeCodeTree.Size = new Size(633, 211);
+            treeCodeTree.TabIndex = 0;
+            treeCodeTree.NodeMouseDoubleClick += treeCodeTree_NodeMouseDoubleClick;
+            treeCodeTree.MouseClick += treeCodeTree_MouseClick;
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSave.Location = new Point(549, 267);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(59, 29);
+            btnSave.TabIndex = 1;
+            btnSave.Text = "上書き";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // txtCode
+            // 
+            txtCode.AcceptsTab = true;
+            txtCode.Dock = DockStyle.Fill;
+            txtCode.Location = new Point(0, 0);
+            txtCode.Multiline = true;
+            txtCode.Name = "txtCode";
+            txtCode.ScrollBars = ScrollBars.Vertical;
+            txtCode.Size = new Size(633, 299);
+            txtCode.TabIndex = 0;
+            txtCode.TextChanged += txtCode_TextChanged;
+            // 
+            // menuCodeMenu
+            // 
+            menuCodeMenu.ImageScalingSize = new Size(20, 20);
+            menuCodeMenu.Items.AddRange(new ToolStripItem[] { ParentAddCodeToolStripMenuItem, ChildAddCodeToolStripMenuItem, EditCodeToolStripMenuItem, CodeDeleteToolStripMenuItem });
+            menuCodeMenu.Name = "menuCodeMenu";
+            menuCodeMenu.Size = new Size(154, 100);
+            // 
+            // ParentAddCodeToolStripMenuItem
+            // 
+            ParentAddCodeToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ParentAddCodeBeforeToolStripMenuItem, ParentAddCodeAfterToolStripMenuItem });
+            ParentAddCodeToolStripMenuItem.Name = "ParentAddCodeToolStripMenuItem";
+            ParentAddCodeToolStripMenuItem.Size = new Size(153, 24);
+            ParentAddCodeToolStripMenuItem.Text = "追加";
+            // 
+            // ParentAddCodeBeforeToolStripMenuItem
+            // 
+            ParentAddCodeBeforeToolStripMenuItem.Name = "ParentAddCodeBeforeToolStripMenuItem";
+            ParentAddCodeBeforeToolStripMenuItem.Size = new Size(122, 26);
+            ParentAddCodeBeforeToolStripMenuItem.Text = "上方";
+            ParentAddCodeBeforeToolStripMenuItem.Click += ParentAddCodeBeforeToolStripMenuItem_Click;
+            // 
+            // ParentAddCodeAfterToolStripMenuItem
+            // 
+            ParentAddCodeAfterToolStripMenuItem.Name = "ParentAddCodeAfterToolStripMenuItem";
+            ParentAddCodeAfterToolStripMenuItem.Size = new Size(122, 26);
+            ParentAddCodeAfterToolStripMenuItem.Text = "下方";
+            ParentAddCodeAfterToolStripMenuItem.Click += ParentAddCodeAfterToolStripMenuItem_Click;
+            // 
+            // ChildAddCodeToolStripMenuItem
+            // 
+            ChildAddCodeToolStripMenuItem.Name = "ChildAddCodeToolStripMenuItem";
+            ChildAddCodeToolStripMenuItem.Size = new Size(153, 24);
+            ChildAddCodeToolStripMenuItem.Text = "追加（子）";
+            ChildAddCodeToolStripMenuItem.Click += CodeChildAddToolStripMenuItem_Click;
+            // 
+            // EditCodeToolStripMenuItem
+            // 
+            EditCodeToolStripMenuItem.Name = "EditCodeToolStripMenuItem";
+            EditCodeToolStripMenuItem.Size = new Size(153, 24);
+            EditCodeToolStripMenuItem.Text = "編集";
+            EditCodeToolStripMenuItem.Click += EditCodeToolStripMenuItem_Click;
+            // 
+            // CodeDeleteToolStripMenuItem
+            // 
+            CodeDeleteToolStripMenuItem.Name = "CodeDeleteToolStripMenuItem";
+            CodeDeleteToolStripMenuItem.Size = new Size(153, 24);
+            CodeDeleteToolStripMenuItem.Text = "削除";
+            CodeDeleteToolStripMenuItem.Click += CodeDeleteToolStripMenuItem_Click;
+            // 
+            // btnAddNewNode
+            // 
+            btnAddNewNode.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnAddNewNode.Location = new Point(502, 144);
+            btnAddNewNode.Name = "btnAddNewNode";
+            btnAddNewNode.Size = new Size(27, 29);
+            btnAddNewNode.TabIndex = 3;
+            btnAddNewNode.Text = "+";
+            btnAddNewNode.UseVisualStyleBackColor = true;
+            btnAddNewNode.Click += btnAddNewNode_Click;
+            // 
+            // btnRemoveNode
+            // 
+            btnRemoveNode.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnRemoveNode.Location = new Point(535, 144);
+            btnRemoveNode.Name = "btnRemoveNode";
+            btnRemoveNode.Size = new Size(27, 29);
+            btnRemoveNode.TabIndex = 4;
+            btnRemoveNode.Text = "-";
+            btnRemoveNode.UseVisualStyleBackColor = true;
+            btnRemoveNode.Click += btnRemoveNode_Click;
+            // 
+            // btnNodeMoveUp
+            // 
+            btnNodeMoveUp.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnNodeMoveUp.Location = new Point(568, 144);
+            btnNodeMoveUp.Name = "btnNodeMoveUp";
+            btnNodeMoveUp.Size = new Size(27, 29);
+            btnNodeMoveUp.TabIndex = 5;
+            btnNodeMoveUp.Text = "↑";
+            btnNodeMoveUp.UseVisualStyleBackColor = true;
+            btnNodeMoveUp.Click += btnNodeMoveUp_Click;
+            // 
+            // btnNodeMoveDown
+            // 
+            btnNodeMoveDown.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnNodeMoveDown.Location = new Point(601, 144);
+            btnNodeMoveDown.Name = "btnNodeMoveDown";
+            btnNodeMoveDown.Size = new Size(27, 29);
+            btnNodeMoveDown.TabIndex = 6;
+            btnNodeMoveDown.Text = "↓";
+            btnNodeMoveDown.UseVisualStyleBackColor = true;
+            btnNodeMoveDown.Click += btnNodeMoveDown_Click;
+            // 
+            // EffectEditor
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(scContainer);
+            Name = "EffectEditor";
+            Size = new Size(633, 514);
+            scContainer.Panel1.ResumeLayout(false);
+            scContainer.Panel2.ResumeLayout(false);
+            scContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)scContainer).EndInit();
+            scContainer.ResumeLayout(false);
+            menuCodeMenu.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer scContainer;
+        private TreeView treeCodeTree;
+        private TextBox txtCode;
+        private ContextMenuStrip menuCodeMenu;
+        private ToolStripMenuItem ChildAddCodeToolStripMenuItem;
+        private ToolStripMenuItem EditCodeToolStripMenuItem;
+        private ToolStripMenuItem CodeDeleteToolStripMenuItem;
+        private Button btnSave;
+        private ToolStripMenuItem ParentAddCodeToolStripMenuItem;
+        private ToolStripMenuItem ParentAddCodeBeforeToolStripMenuItem;
+        private ToolStripMenuItem ParentAddCodeAfterToolStripMenuItem;
+        private Button btnSaveFile;
+        private Button btnNodeMoveDown;
+        private Button btnNodeMoveUp;
+        private Button btnRemoveNode;
+        private Button btnAddNewNode;
+    }
+}
---
+++ EOProcesser/EffectEditor.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EOProcesser
+{
+    public partial class EffectEditor : UserControl
+    {
+        public EffectEditor()
+        {
+            InitializeComponent();
+        }
+        public void LoadCode(string str, bool isFile = false)
+        {
+            if (!isFile)
+            {
+                CurrentFile = null;
+            }
+            treeCodeTree.Nodes.Clear();
+            var ml = ERACodeAnalyzer.AnalyzeCode(str);
+            treeCodeTree.Nodes.AddRange([.. ml.GetTreeNodes()]);
+            txtCode.Text = str;
+            TextHasChanged = false;
+        }
+        public string? CurrentFile = null;
+        public void LoadCodeFromFile(string file)
+        {
+            string content = File.ReadAllText(file);
+            LoadCode(content, true);
+            CurrentFile = file;
+        }
+        public void SaveToFile(string file)
+        {
+            StringBuilder sb = new();
+            foreach (TreeNode node in treeCodeTree.Nodes)
+            {
+                sb.Append(node.Tag.ToString());
+            }
+        }
+        public void SaveToFile()
+        {
+            if (CurrentFile != null)
+            {
+                SaveToFile(CurrentFile);
+            }
+        }
+
+        private void treeCodeTree_MouseClick(object sender, MouseEventArgs e)
+        {
+            var node = treeCodeTree.GetNodeAt(e.X, e.Y);
+            if (e.Button == MouseButtons.Right && node != null)
+            {
+                // Check if the selected node is an ERACodeLine and disable the add child menu item if it is
+
+                ChildAddCodeToolStripMenuItem.Enabled = node.Tag is ERABlockSegment;
+
+                // Show context menu at the current mouse position
+                treeCodeTree.SelectedNode = node;
+                menuCodeMenu.Show(treeCodeTree, e.Location);
+            }
+        }
+
+        private void CodeChildAddToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (treeCodeTree.SelectedNode.Tag is ERACodeLine)
+            {
+                MessageBox.Show("単一行のコードに子要素を追加することはできません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            ERACodeGenericLine line = new("");
+            treeCodeTree.SelectedNode.Nodes.AddRange([.. line.GetTreeNodes()]);
+        }
+        private void ParentAddCodeBeforeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (treeCodeTree.SelectedNode != null && treeCodeTree.SelectedNode.Parent != null)
+            {
+                ERACodeGenericLine line = new("");
+                TreeNode[] newNodes = [.. line.GetTreeNodes()];
+                int index = treeCodeTree.SelectedNode.Index;
+                TreeNode parent = treeCodeTree.SelectedNode.Parent;
+
+                // Insert before the selected node
+                foreach (var node in newNodes)
+                {
+                    parent.Nodes.Insert(index, node);
+                }
+            }
+        }
+
+        private void ParentAddCodeAfterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (treeCodeTree.SelectedNode != null && treeCodeTree.SelectedNode.Parent != null)
+            {
+                ERACodeGenericLine line = new("");
+                TreeNode[] newNodes = [.. line.GetTreeNodes()];
+                int index = treeCodeTree.SelectedNode.Index + 1;
+                TreeNode parent = treeCodeTree.SelectedNode.Parent;
+
+                // Insert after the selected node
+                foreach (var node in newNodes)
+                {
+                    parent.Nodes.Insert(index++, node);
+                }
+            }
+        }
+
+        private void CodeDeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (treeCodeTree.SelectedNode != null)
+            {
+                DialogResult result = MessageBox.Show(
+                    "削除してもよろしいですか？",
+                    "確認",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    treeCodeTree.SelectedNode.Remove();
+                }
+            }
+        }
+
+        TreeNode? CurrentEditingNode = null;
+
+        private void EditCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var node = treeCodeTree.SelectedNode;
+            if (node == null)
+            {
+                return;
+            }
+            if (TextHasChanged)
+            {
+                DialogResult result = MessageBox.Show(
+                    "コードが変更されています。保存しますか？",
+                    "保存確認",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    btnSave_Click(sender, e);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            CurrentEditingNode = node;
+            string code = CurrentEditingNode.Tag.ToString() ?? "";
+            
+            // Split the code into lines
+            string[] lines = code.Split(["\r\n", "\n"], StringSplitOptions.None);
+            
+            // Find the minimum number of leading tabs
+            int minTabCount = int.MaxValue;
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                
+                int tabCount = 0;
+                foreach (char c in line)
+                {
+                    if (c == '\t') tabCount++;
+                    else break;
+                }
+                
+                if (tabCount < minTabCount)
+                {
+                    minTabCount = tabCount;
+                }
+            }
+            
+            // If we found some tabs to trim
+            if (minTabCount > 0 && minTabCount < int.MaxValue)
+            {
+                // Remove the minimum number of tabs from each line
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(lines[i]))
+                    {
+                        lines[i] = lines[i].Substring(Math.Min(minTabCount, lines[i].TakeWhile(c => c == '\t').Count()));
+                    }
+                }
+                
+                // Join the lines back together
+                code = string.Join(Environment.NewLine, lines);
+            }
+            
+            txtCode.Text = code;
+            TextHasChanged = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var codes = ERACodeAnalyzer.AnalyzeCode(txtCode.Text);
+            List<TreeNode> nodes = codes.GetTreeNodes();
+
+            if (CurrentEditingNode != null && nodes.Count > 0)
+            {
+                TreeNode parentNode = CurrentEditingNode.Parent;
+                int index = CurrentEditingNode.Index;
+
+                CurrentEditingNode.Remove();
+
+                if (parentNode != null)
+                {
+                    foreach (var node in nodes)
+                    {
+                        parentNode.Nodes.Insert(index++, node);
+                    }
+
+                    // 更新父节点及其所有上级节点的Tag属性
+                    RefreshParentNodesTags(parentNode);
+                }
+                else
+                {
+                    treeCodeTree.Nodes.Clear();
+                    treeCodeTree.Nodes.AddRange([.. nodes]);
+                }
+
+                treeCodeTree.SelectedNode = nodes[0];
+                EditCodeToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// 递归更新节点及其所有父节点的Tag属性
+        /// </summary>
+        /// <param name="node">要更新的节点</param>
+        private void RefreshParentNodesTags(TreeNode node)
+        {
+            if (node == null) return;
+
+            // 如果节点的Tag是ERACode类型，则更新它
+            if (node.Tag is ERACode eraCode)
+            {
+                // 获取当前节点的所有子节点列表
+                List<TreeNode> childNodes = new List<TreeNode>();
+                foreach (TreeNode childNode in node.Nodes)
+                {
+                    childNodes.Add(childNode);
+                }
+
+                // 调用ERACode的RefreshFromChildTreeNodes方法更新Tag
+                if (node.Tag is ERABlockSegment block)
+                {
+                    block.RefreshFromChildTreeNodes(childNodes);
+                }
+
+                // 递归处理父节点
+                RefreshParentNodesTags(node.Parent);
+            }
+        }
+
+        bool TextHasChanged = false;
+
+        private void txtCode_TextChanged(object sender, EventArgs e)
+        {
+            TextHasChanged = true;
+        }
+
+        private void treeCodeTree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            EditCodeToolStripMenuItem_Click(sender, e);
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            if (CurrentFile != null)
+            {
+                SaveToFile();
+            }
+        }
+
+        private void btnAddNewNode_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeCodeTree.SelectedNode;
+
+            if (selectedNode == null)
+            {
+                // If no node is selected, add to the root
+                ERACodeGenericLine line = new("");
+                treeCodeTree.Nodes.AddRange([.. line.GetTreeNodes()]);
+            }
+            else if (selectedNode.Tag is ERABlockSegment)
+            {
+                // Add as a child if the selected node is a block
+                ERACodeGenericLine line = new("");
+                selectedNode.Nodes.AddRange([.. line.GetTreeNodes()]);
+                selectedNode.Expand();
+            }
+            else if (selectedNode.Parent != null)
+            {
+                // Add after the selected node if it's not a block
+                ERACodeGenericLine line = new("");
+                TreeNode[] newNodes = [.. line.GetTreeNodes()];
+                int index = selectedNode.Index + 1;
+                TreeNode parent = selectedNode.Parent;
+
+                foreach (var node in newNodes)
+                {
+                    parent.Nodes.Insert(index++, node);
+        }
+                
+                RefreshParentNodesTags(parent);
+            }
+        }
+
+        private void btnRemoveNode_Click(object sender, EventArgs e)
+        {
+            if (treeCodeTree.SelectedNode != null)
+            {
+                DialogResult result = MessageBox.Show(
+                    "削除してもよろしいですか？",
+                    "確認",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    TreeNode parent = treeCodeTree.SelectedNode.Parent;
+                    treeCodeTree.SelectedNode.Remove();
+                    
+                    if (parent != null)
+                    {
+                        RefreshParentNodesTags(parent);
+                    }
+                }
+            }
+        }
+
+        private void btnNodeMoveUp_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeCodeTree.SelectedNode;
+            if (selectedNode != null && selectedNode.Parent != null)
+            {
+                TreeNode parent = selectedNode.Parent;
+                int index = selectedNode.Index;
+                
+                if (index > 0)
+                {
+                    parent.Nodes.RemoveAt(index);
+                    parent.Nodes.Insert(index - 1, selectedNode);
+                    treeCodeTree.SelectedNode = selectedNode;
+                    RefreshParentNodesTags(parent);
+                }
+            }
+        }
+
+        private void btnNodeMoveDown_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = treeCodeTree.SelectedNode;
+            if (selectedNode != null && selectedNode.Parent != null)
+            {
+                TreeNode parent = selectedNode.Parent;
+                int index = selectedNode.Index;
+                
+                if (index < parent.Nodes.Count - 1)
+                {
+                    parent.Nodes.RemoveAt(index);
+                    parent.Nodes.Insert(index + 1, selectedNode);
+                    treeCodeTree.SelectedNode = selectedNode;
+                    RefreshParentNodesTags(parent);
+                }
+            }
+        }
+    }
+}
